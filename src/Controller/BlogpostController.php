9f15fd4c8148dbb0e648390f57228b99b94c78e3
adelinelucas <eon.adelineeon@gmail.com>
2021-10-12 16:11:19 +0200

    ajout actualites + realisations + pagination

---
+++ src/Controller/BlogpostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BlogpostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BlogpostController extends AbstractController
+{
+    /**
+     * @Route("/actualites", name="actualites")
+     */
+    public function actualites(
+        Request $request,
+        BlogpostRepository $blogpostRepository,
+        PaginatorInterface $paginator
+    ): Response
+    {
+        $data = $blogpostRepository->findAll();
+
+        $actualites = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            6
+        );
+        return $this->render('blogpost/actualites.html.twig', [
+            'actualites' => $actualites,
+        ]);
+    }
+}
---
+++ src/Controller/PatisserieController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PatisserieRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PatisserieController extends AbstractController
+{
+    /**
+     * @Route("/realisations", name="realisations")
+     */
+    public function realisations(
+        Request $request,
+        PatisserieRepository $patisserieRepository,
+        PaginatorInterface $paginator
+    ): Response{
+        $data = $patisserieRepository->findAll();
+
+        $patisseries = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            6
+        );
+
+        return $this->render('patisserie/realisations.html.twig', [
+            'patisseries' => $patisseries,
+        ]);
+    }
+}
