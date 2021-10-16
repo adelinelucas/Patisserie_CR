a1ecc0da89cd5bc380bfba7723c04a4c1b4fc62e
adelinelucas <eon.adelineeon@gmail.com>
2021-10-16 22:29:41 +0200

    nt

--- tests/ContactFunctionnalTest.php
+++ tests/ContactFunctionnalTest.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App\Tests;
+// namespace App\Tests;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+// use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ContactFunctionnalTest extends WebTestCase
-{
-    public function testShouldDisplayContact()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/contact');
+// class ContactFunctionnalTest extends WebTestCase
+// {
+//     public function testShouldDisplayContact()
+//     {
+//         $client = static::createClient();
+//         $crawler = $client->request('GET', '/contact');
 
-        $this->assertResponseIsSuccessful();
-    }
-}
+//         $this->assertResponseIsSuccessful();
+//     }
+// }
